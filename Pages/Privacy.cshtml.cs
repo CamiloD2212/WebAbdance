a6c6002b92c06b94611b4fc41b7f22fefc4dff14
CamiloD2212 <camilod2212@gmail.com>
2026-07-14 01:34:40 -0300

    Inicio como gta6 pero para abdance terminado

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAbdance.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
